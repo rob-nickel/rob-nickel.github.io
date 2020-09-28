f1509bb4f87503e75b37d0de161b225f19a84fac
Rob Nickel <42808959+rob-nickel@users.noreply.github.com>
2020-09-28 00:05:26 -0500

    Static website. Initial framework.

---
+++ precache-manifest.634f934c754081332f01655c35cebbfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3073b49eff8ad789062a65396316160c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c06db6c1eeadb71c7c44",
+    "url": "/static/css/main.ff220bff.chunk.css"
+  },
+  {
+    "revision": "6d56ef5d9cd9fb321875",
+    "url": "/static/js/2.f78eb249.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f78eb249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06db6c1eeadb71c7c44",
+    "url": "/static/js/main.9eb03840.chunk.js"
+  },
+  {
+    "revision": "12120af6d44a5d6c4428",
+    "url": "/static/js/runtime-main.2651ac13.js"
+  }
+]);
